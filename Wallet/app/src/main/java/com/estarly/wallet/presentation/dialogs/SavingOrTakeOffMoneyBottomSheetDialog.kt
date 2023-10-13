ca1edf7d527aa59239201686be391c28ff9f6183
estarly.amaya <estarly.amaya@servinformacion.com>
2023-10-12 23:54:58 -0500

    IMPLEMENTATION
    - Logic was implemented to withdraw money from savings.

--- Wallet/app/src/main/java/com/estarly/wallet/presentation/dialogs/SavingOrTakeOffMoneyBottomSheetDialog.kt
+++ Wallet/app/src/main/java/com/estarly/wallet/presentation/dialogs/SavingOrTakeOffMoneyBottomSheetDialog.kt
@@ -2,22 +2,17 @@ package com.estarly.wallet.presentation.dialogs
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.estarly.wallet.R
 import com.estarly.wallet.domain.models.DistributionOfMoneyModel
-import com.estarly.wallet.domain.models.GoalModel
-import com.estarly.wallet.presentation.adapters.DistributionSelectAdapter
 import com.estarly.wallet.utils.formatSalary
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
-class SavingMoneyBottomSheetDialog {
+class SavingOrTakeOffMoneyBottomSheetDialog {
     companion object {
         private var distribution : DistributionOfMoneyModel? = null
         private var availableMoney : Double = 0.0
@@ -27,20 +22,29 @@ class SavingMoneyBottomSheetDialog {
             availableMoney : Double,
             onDismiss: () -> Unit,
             onSavingMoney: (Double) -> Unit,
+            takeOfMoney : Boolean = false
         ) {
             this.availableMoney = availableMoney
             val bottomSheetDialog = BottomSheetDialog(activity)
             val view = activity.layoutInflater.inflate(R.layout.dialog_to_saving_money, null)
             val amount = view.findViewById<EditText>(R.id.edtAmount)
+            val btnSavingMoney = view.findViewById<Button>(R.id.btnSavingMoney)
             view.findViewById<ImageButton>(R.id.btnCloseDialog).setOnClickListener { bottomSheetDialog.dismiss() }
+            if(takeOfMoney){
+                val txtTitle = view.findViewById<TextView>(R.id.txtTitleSavingMoney)
+                val txtContent = view.findViewById<TextView>(R.id.txtContentSavingMoney)
+                txtTitle.text = "¿Cuánto dinero deseas sacar?"
+                txtContent.text = "¡Haz realidad tus planes! Retira la suma que necesitas de tus ahorros, liberándola para usarla como quieras en tu saldo disponible."
+                btnSavingMoney.text = "Retirar"
+            }
             amount.addTextChangedListener {
                 if(it.toString().isEmpty()) return@addTextChangedListener
                 amount.error = if(availableMoney < it.toString().toDouble()){
                     "El monto disponible es ${availableMoney.formatSalary()}"
                 } else null
             }
-            view.findViewById<Button>(R.id.btnSavingMoney).setOnClickListener {
-                if(amount.error != null) return@setOnClickListener
+            btnSavingMoney.setOnClickListener {
+                if(amount.error != null || amount.text.toString().isEmpty()) return@setOnClickListener
                 onSavingMoney(amount.text.toString().toDouble())
                 bottomSheetDialog.dismiss()
             }
--- Wallet/app/src/main/java/com/estarly/wallet/presentation/viewmodels/SavingMoneyViewModel.kt
+++ Wallet/app/src/main/java/com/estarly/wallet/presentation/viewmodels/SavingMoneyViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.estarly.wallet.domain.usescases.GetSavingMoneyUseCase
+import com.estarly.wallet.domain.usescases.TakeMoneyOfSavingMoneyUseCase
 import com.estarly.wallet.utils.formatSalary
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,14 +13,25 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SavingMoneyViewModel @Inject constructor(
-    private val getSavingMoneyUseCase : GetSavingMoneyUseCase
+    private val getSavingMoneyUseCase : GetSavingMoneyUseCase,
+    private val takeMoneyOfSavingMoneyUseCase : TakeMoneyOfSavingMoneyUseCase
 ): ViewModel() {
     private var _amountSaving = MutableLiveData<String>()
     var amountSaving : LiveData<String> = _amountSaving
+    var amountAvailable : Double = 0.0
 
     fun getAmountSaving(){
         viewModelScope.launch {
-            _amountSaving.value = getSavingMoneyUseCase()?.amountSavingFormatted ?: "0"
+            val response = getSavingMoneyUseCase()
+            _amountSaving.value = response?.amountSavingFormatted ?: "0"
+            amountAvailable = response?.amountSaving ?: 0.0
+        }
+    }
+
+    fun retireMoney(money: Double) {
+        viewModelScope.launch {
+            takeMoneyOfSavingMoneyUseCase(money)
+            getAmountSaving()
         }
     }
 }
