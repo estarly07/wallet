7f994ebac8c523bdb77842bd723c19a56d3f91e7
estarly.amaya <estarly.amaya@servinformacion.com>
2024-03-21 13:18:27 -0500

    IMPLEMENTATION
    - Implemented new icons for the dropdown menu
    - Bug fixed in the automation adapter

--- Wallet/app/src/main/java/com/estarly/wallet/domain/models/DistributionOfMoneyModel.kt
+++ Wallet/app/src/main/java/com/estarly/wallet/domain/models/DistributionOfMoneyModel.kt
@@ -7,5 +7,5 @@ data class DistributionOfMoneyModel(
     val name        : String,
     val image       : Int,
     val amountExpected : Double,
-    val amountExpectedFormatted : String = ""
+    var amountExpectedFormatted : String = ""
 )
--- Wallet/app/src/main/java/com/estarly/wallet/presentation/adapters/DistributionAutomationAdapter.kt
+++ Wallet/app/src/main/java/com/estarly/wallet/presentation/adapters/DistributionAutomationAdapter.kt
@@ -1,5 +1,6 @@
 package com.estarly.wallet.presentation.adapters
 
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
@@ -12,7 +13,7 @@ class DistributionAutomationAdapter(
     private val list: List<DistributionOfMoneyModel>,
     val addTextEditChange: (value:DistributionOfMoneyModel, position: Int,text: String)-> Unit,
 ) : RecyclerView.Adapter<DistributionAutomationAdapter.Holder>() {
-    class Holder(val binding: ItemDistributionAutomationBinding) : ViewHolder(binding.root)
+    class Holder(val binding: ItemDistributionAutomationBinding, var textWatcher: TextWatcher? = null) : ViewHolder(binding.root)
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder
@@ -23,9 +24,11 @@ class DistributionAutomationAdapter(
     override fun onBindViewHolder(holder: Holder, position: Int) {
         with(holder.binding){
             val distribution = list[position]
+            holder.textWatcher?.let { edtExpectedQuantity.removeTextChangedListener(holder.textWatcher) }
             edtExpectedQuantity.setText(distribution.amountExpectedFormatted)
-            edtExpectedQuantity.addTextChangedListener {
+            holder.textWatcher = edtExpectedQuantity.addTextChangedListener {
                 addTextEditChange(distribution,position,it.toString())
+                distribution.amountExpectedFormatted = it.toString()
             }
             txtTitleDistributionAutomation.text = distribution.name
             imgBackgroundDistribution.setImageResource(distribution.image)
